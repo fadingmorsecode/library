c2f447d1ec233236dcd5d2cf6ec2abc783bb9fc9
Drake Williamson <williamson.drake@outlook.com>
2023-04-28 01:36:12 -0700

    Create function to construct book objects and add to library array

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let myLibrary = [];
 
-function book() {
-    
+function book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function addBookToLibrary() {
-
+    let bookTitle = prompt('Please enter book title', 'Title');
+    let bookAuthor = prompt('Please enter author', 'Author');
+    let bookPages = prompt('Please enter number of pages', 'Pages');
+    let newBook = new book(`${bookTitle}`, `${bookAuthor}`, `${bookPages}`);
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary();
