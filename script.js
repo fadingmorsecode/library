12e2edcda3df0a8772ad15b9c788e08f02552162
Drake Williamson <williamson.drake@outlook.com>
2023-04-28 17:31:22 -0700

    Create dummy book objects and initial html structure to house cards

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-let myLibrary = [];
+let myLibrary = [{
+    title: 'The Grapes of Wrath',
+    author: 'John Steinback',
+    pages: '530'
+},
+{
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    pages: '130'
+},
+{
+    title: 'Ethan Frome',
+    author: 'Edith Wharton',
+    pages: '195'
+},
+{
+    title: 'Farenheit 451',
+    author: 'Ray Bradbury',
+    pages: '158'
+}
+];
 
 function book(title, author, pages) {
     this.title = title;
@@ -6,12 +26,15 @@ function book(title, author, pages) {
     this.pages = pages;
 }
 
+/*
 function addBookToLibrary() {
     let bookTitle = prompt('Please enter book title', 'Title');
     let bookAuthor = prompt('Please enter author', 'Author');
     let bookPages = prompt('Please enter number of pages', 'Pages');
     let newBook = new book(`${bookTitle}`, `${bookAuthor}`, `${bookPages}`);
     myLibrary.push(newBook);
-}
+
 
 addBookToLibrary();
+*/
+console.log(myLibrary);
